6c2c123e3b6fc509da06e366e7004cadcef5063d
aa_trusov@bingo-boom.com <aa_trusov@bingo-boom.com>
2021-04-02 23:51:05 +0300

    -adding layers

--- src/components/LayersContainer/index.js
+++ src/components/LayersContainer/index.js
@@ -5,14 +5,29 @@ import { ResizeObserver } from '@juggle/resize-observer';
 
 function LayersContainer(props) {
 
+    const layerContainer = useRef(null)
     const [actualResolution, resolutionChange] = useState(null)
 
     const {layers, creation} = props;
+
+    const startCreation = ({clientX, clientY}, type) => {
+        const x = clientX - 4;
+        const y = clientY - 52;
+       
+    }
+
+    const creationInProgress = ( {clientX, clientY}, type) => {
+
+    }
+
+    const finishCreation = ({clientX, clientY}, type) => {
+
+    }
     useEffect(() => {
         const ro = new ResizeObserver((entries, observer) => {
             resolutionChange({
-                width: (document.documentElement.clientWidth - 4) * 0.8 ,
-                height: (document.documentElement.clientHeight - 60 )
+                width: Math.floor((document.documentElement.clientWidth ) * 0.8) ,
+                height: Math.floor((document.documentElement.clientWidth ) * 0.8 * 9 / 16)
             })
         });
         const observerOptions = {
@@ -24,8 +39,18 @@ function LayersContainer(props) {
         }
     }, []);
 
+    useEffect(() => {
+        if(creation.creatingObject){
+
+        }
+    }, [creation]);
+
+
     return (
-        <div className={"layers-container" + (creation.creatingObject ? " selected" : "")}>
+        <div onPointerDown={(event) => startCreation(event, creation.creatingObject)}
+             onPointerUp={(event) => finishCreation(event,creation.creatingObject)}
+             onPointerMove={(event) => creationInProgress(event, creation.creatingObject)}
+             ref={layerContainer} className={"layers-container" + (creation.creatingObject ? " selected" : "")}>
 
         </div>
     );
